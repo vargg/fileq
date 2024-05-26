14f84e2362b9df1ad27e1bdeb28c9865eec001b0
vargg <smrglwp@outlook.com>
2024-05-26 10:46:57 +0300

    replace magic values ​​with constants

--- src/processing.c
+++ src/processing.c
@@ -7,6 +7,12 @@
 #include <unistd.h>
 
 
+const char *work_dir_name = ".fileq";
+const char *qs_subdir_name = "qs";
+const u_int8_t batch_size = 64;
+const u_int8_t max_num_of_skipped_items = 16;
+
+
 typedef struct {
     u_int64_t head;  // position of the first message
     u_int64_t skipped;  // number of messages pulled but not deleted yet
@@ -25,24 +31,22 @@ typedef struct {
 
 char *get_work_dir_path() {
     char *home_dir = getenv("HOME");
-    char *dir_name = ".fileq";
 
-    char *full_dir_path = malloc(strlen(dir_name) + strlen(home_dir) + 2);
+    char *full_dir_path = malloc(strlen(home_dir) + strlen(work_dir_name) + 2);
     strcpy(full_dir_path, home_dir);
     strcat(full_dir_path, "/");
-    strcat(full_dir_path, dir_name);
+    strcat(full_dir_path, work_dir_name);
 
     return full_dir_path;
 }
 
 char *get_qs_dir_path() {
     char *work_dir = get_work_dir_path();
-    char *dir_name = "qs";
 
-    char *full_dir_path = malloc(strlen(dir_name) + strlen(work_dir) + 2);
+    char *full_dir_path = malloc(strlen(work_dir) + strlen(qs_subdir_name) + 2);
     strcpy(full_dir_path, work_dir);
     strcat(full_dir_path, "/");
-    strcat(full_dir_path, dir_name);
+    strcat(full_dir_path, qs_subdir_name);
 
     free(work_dir);
 
@@ -64,7 +68,7 @@ char *get_q_path(char *q_name) {
 }
 
 
-int check_dir(char *path) {
+int check_or_make_dir(char *path) {
     struct stat st = {0};
 
     int code = EXIT_SUCCESS;
@@ -77,24 +81,24 @@ int check_dir(char *path) {
 
 
 int make_dirs() {
-    int code = check_dir(get_work_dir_path());
+    int code = check_or_make_dir(get_work_dir_path());
 
     if (code == EXIT_SUCCESS) {
-        code = check_dir(get_qs_dir_path());
+        code = check_or_make_dir(get_qs_dir_path());
     }
 
     return code;
 }
 
-
+// remove extracted messages from file head
 void _trim(FILE *file, FileMetadata *metadata) {
     u_int64_t shift = metadata_size;
 
-    char *message = malloc(64);
+    char *message = malloc(batch_size);
     while (1) {
         fseek(file, metadata->head + shift - metadata_size, SEEK_SET);
 
-        if (fgets(message, 64, file) == NULL) {
+        if (fgets(message, batch_size, file) == NULL) {
             break;
         }
 
@@ -247,13 +251,13 @@ int print_all_messages_from_queue(char *q_name) {
         return EXIT_FAILURE;
     }
 
-    char *message = malloc(64);
+    char *message = malloc(batch_size);
     if (fseek(queue->file, queue->metadata->head, SEEK_SET) != 0) {
         printf("failed to seek\n");
         return EXIT_FAILURE;
     }
 
-    while (fgets(message, 64, queue->file) != NULL) {
+    while (fgets(message, batch_size, queue->file) != NULL) {
         printf("%s", message);
     }
 
@@ -276,7 +280,7 @@ int pull_message_from_queue(char *q_name) {
         return EXIT_FAILURE;
     }
 
-    char *message = malloc(64);
+    char *message = malloc(batch_size);
     if (fseek(queue->file, queue->metadata->head, SEEK_SET) != 0) {
         printf("failed to seek\n");
         return EXIT_FAILURE;
@@ -284,7 +288,7 @@ int pull_message_from_queue(char *q_name) {
 
     int shift = 0;
     while (1) {
-        fgets(message, 64, queue->file);
+        fgets(message, batch_size, queue->file);
         printf("%s", message);
         shift += strlen(message);
         if (message[strlen(message) - 1] == '\n') {
@@ -298,7 +302,7 @@ int pull_message_from_queue(char *q_name) {
     queue->metadata->skipped++;
     queue->metadata->size--;
 
-    if (queue->metadata->skipped > 5) {
+    if (queue->metadata->skipped > max_num_of_skipped_items) {
         _trim(queue->file, queue->metadata);
     }
 
